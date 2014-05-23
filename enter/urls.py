338143a2c2697223aa80c2f57b400fe561d0c53a
Alex Carlin <carlin@ucdavis.edu>
2014-05-22 23:41:19 -0700

    Added various pages

--- enter/urls.py
+++ enter/urls.py
@@ -4,4 +4,6 @@ from enter import views
 
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
+  url(r'^browse/', views.browse, name='browse'),
+  url(r'^submit/', views.submit, name='submit'),
 )
--- enter/views.py
+++ enter/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from enter.models import Entry
 
-def index(request):
+def browse(request):
   entry_list = Entry.objects.order_by('-entry_ID')
-  return render(request, 'enter/index.html', {'entry_list': entry_list})
+  return render(request, 'enter/browse.html', {'entry_list': entry_list})
+
+def index(request):
+  return render(request, 'enter/index.html')
+
+def submit(request):
+  entry = get_list_or_404(Entry)
+  return render(request, 'enter/submit.html', {'entry': entry})
