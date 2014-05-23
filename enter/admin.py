ea7823bed977f3e2a149f20b8bea40a14f511bc0
Alex Carlin <carlin@ucdavis.edu>
2014-05-22 22:14:39 -0700

    Added HTML and CSS

--- enter/admin.py
+++ enter/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from enter.models import Entry
+
+admin.site.register(Entry)
 
 # Register your models here.
--- enter/views.py
+++ enter/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from enter.models import Entry
 
-# Create your views here.
+def index(request):
+  entry_list = Entry.objects.order_by('-entry_ID')
+  return render(request, 'enter/index.html', {'entry_list': entry_list})
