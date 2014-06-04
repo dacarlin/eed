5d143765db8fb9406ee9c16d2479fbe5797656cc
Alex Carlin <carlin@ucdavis.edu>
2014-06-04 00:33:01 -0700

    Added redirect to enter/

--- eed/urls.py
+++ eed/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 from django.contrib import admin
 from enter import views
 admin.autodiscover()
@@ -7,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'eed.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', lambda r : HttpResponseRedirect('enter/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^enter/', include('enter.urls')),
 )
