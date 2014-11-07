b77496ee9285b14fbfe1e7bc34d4305de7f95d52
Alex Carlin <carlin@ucdavis.edu>
2014-11-07 08:58:34 -0800

    Updated cookbook

--- enter/views.py
+++ enter/views.py
@@ -6,19 +6,15 @@ from enter.models import DataEntry, DataEntryForm
 
 def adam(request):
   if request.method == 'POST':
-    form = DataEntryForm(request.POST)    
-    print(dir(form))
+    form = DataEntryForm(request.POST)
     if form.is_valid():
       analyses = form.process()
-      # add in variables 
-      print(dir(form))
-      print(form.fields())
       return render(request, 'enter/preview.html', { 'analyses': analyses, 'form': form, } )
     else:
       return render(request, 'enter/cookbook.html', { 'form': form, } )
   else:
     form = DataEntryForm()
-    return render(request, 'enter/cookbook.html', {'form': form} ) 
+    return render(request, 'enter/cookbook.html', {'form': form} )
 
 def thanks(request):
   form = DataEntryForm(request.POST)
@@ -28,12 +24,13 @@ def thanks(request):
     return render(request, 'enter/thanks.html', {'':''} )
   else:
     return render(request, 'enter/cookbook.html', {'form': form} ) #empty form
-    
+
 def browse(request):
   #entry_list = DataEntry.objects.all().order_by('-pub_date')
   entry_list = DataEntry.objects.all()
+  #entry_lits = Entry.objects.all()
   return render(request, 'enter/browse.html', {'entry_list': entry_list})
-  
+
 def index(request):
   return render(request, 'enter/index.html')
 
