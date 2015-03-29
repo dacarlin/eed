45800f8713ba9b123afcbdd69375aa67d021e929
Alex Carlin <carlin@ucdavis.edu>
2015-03-29 13:42:53 -0700

    Major changes to views
    Started storing plots in database, added a bunch of mutants (whole Bagel data set actually), improvements to style, added metadata rows to browse view

--- app/views.py
+++ app/views.py
@@ -19,25 +19,40 @@ def adam(request):
 def thanks(request):
   if request.method == 'POST':
     form = DataEntryForm(request.POST)
+    
     if form.is_valid():
-      analyses = form.process()
-      for sample in analyses.keys():
+      analyses = form.process() # dict analyses = form.process( self )
+      
+      # process the form before saving 
+      for sample, dic in analyses.items():
         entry = Entry()
         entry.mutant = sample
-        entry.__dict__.update(analyses[sample])
+        entry.mm = dic['mm_plot']
+        entry.lin = dic['linear_plot']
+        entry.__dict__.update( analyses[sample] )
         entry.save()      
       return render(request, 'app/thanks.html', { 'analyses': analyses, 'form': form, } )
-    else:
+    
+    else: # form isn't valid
+      # why? bad password? 
       return render(request, 'app/error.html', { 'form': form, } )
+      
   else:
-    return redirect(index)
+    return redirect(index) # Why redirect to home here? 
 
 def browse(request):
+  # clearly this could get a lot better
+  
+  # get all Entry objects and order by newest first 
   entry_list = Entry.objects.all().order_by('-date')
-  return render(request, 'app/browse.html', {'entry_list': entry_list })
+  
+  # pass to the template browse.html as a dict 
+  return render( request, 'app/browse.html', { 'entry_list': entry_list } )
+  # I guess django.shortcuts.render( object request, path template, dict stuff for template )
 
 def index(request):
-  return render(request, 'app/index.html')
+  # should serve these as flat HTML from /static/
+  return render( request, 'app/index.html' )
 
 def help(request):
-  return render(request, 'app/help.html')
+  return render( request, 'app/help.html' )
